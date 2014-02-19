819ef5455fdd34bf5b685d89c6378b7c0a838510
Shannon Eric Peevey <peeveys@unlv.nevada.edu>
2014-02-19 10:09:25 -0800

    adding slider abilities

--- js/slider.js
+++ js/slider.js
@@ -1,8 +1,10 @@
 //This document contains code only for the sliders
-
+var seconds = 20000;
+var ease = 'easeInQuad';
 
 $(document).ready(function(){
 	//slider 
+	
 
 	$('#slider').slider({
 		value: 0,   //starting point of slider
@@ -11,14 +13,17 @@ $(document).ready(function(){
 		step: 10,   //snap to grid amount
 		animate: 'slow',
 		slide: function( event, ui ) { //put text at label
-		$( ".label1" ).text(ui.value );
+		//$( ".label1" ).text(ui.value );
 		},
 		change: function(event, ui){
-		if (ui.value == 10){
-			$("#femalemonster img").fadeIn(2000).attr('src',"images/FemaleMonster/female_g_body.png");
+		if (ui.value == 0){
+			$("#femalemonster img").fadeIn(seconds,ease).attr('src',"images/FemaleMonster/female_b_body.png");
    			return false;
-		}else if(ui.value == 0){
-			$("#femalemonster img").fadeIn(2000).attr('src',"images/FemaleMonster/female_b_body.png");
+		}else if(ui.value == 10){
+			$("#femalemonster img").fadeIn(seconds, ease).attr('src',"images/FemaleMonster/female_bg_body.png");
+   			return false;
+		}else if(ui.value == 20){
+			$("#femalemonster img").fadeIn(seconds, ease).attr('src',"images/FemaleMonster/female_g_body.png");
    			return false;
 		}
 	}
