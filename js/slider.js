901f8d6530bf216ac4dc9af12b1158a22a00b36f
Shannon Eric Peevey <peeveys@unlv.nevada.edu>
2014-02-19 12:09:54 -0800

    adding sliders

--- js/slider.js
+++ js/slider.js
@@ -1,12 +1,45 @@
 //This document contains code only for the sliders
-var seconds = 20000;
-var ease = 'easeInQuad';
+var seconds = 'linear';
+var ease = 'linear';
+var overlay = 1;
 
 $(document).ready(function(){
-	//slider 
 	
-
+	//show female default
+	$('#blue-body').show();
+	
+	
+	//slider one -- skin color
 	$('#slider').slider({
+		value: 0,   //starting point of slider
+		min: 0,
+		max: 20,   //ending amount
+		step: 10,   //snap to grid amount
+		animate: 'slow',
+		slide: function( event, ui ) { //put text at label
+		//$( ".label1" ).text(ui.value );
+		},
+		change: function(event, ui){
+		if (ui.value == 0){
+			$("#green-body").hide();
+			$("#blue-green-body").hide();
+			$("#blue-body").fadeIn(seconds).show();
+   			return false;
+		}else if(ui.value == 10){
+			$("#blue-body").hide();
+			$("#green-body").hide();
+			$("#blue-green-body").fadeIn(seconds).show();
+   			return false;
+		}else if(ui.value == 20){
+			$("#blue-body").hide();
+			$("#blue-green-body").hide();
+			$("#green-body").fadeIn(seconds).show();
+   			return false;
+		}
+	}
+	});
+	
+/*		$('#slider').slider({
 		value: 0,   //starting point of slider
 		min: 0,
 		max: 20,   //ending amount
@@ -27,6 +60,69 @@ $(document).ready(function(){
    			return false;
 		}
 	}
+	});*/
+	
+	//slider two -- tail
+	$('#slider2').slider({
+		value: 0,   //starting point of slider
+		min: 0,
+		max: 20,   //ending amount
+		step: 10,   //snap to grid amount
+		animate: 'slow',
+		slide: function( event, ui ) { //put text at label
+		//$( ".label1" ).text(ui.value );
+		},
+		change: function(event, ui){
+		
+		$('.tail').css('z-index','1');	
+		
+		if (ui.value == 0){
+			$("#p_straight_tail").hide();
+			$("#o_straight_tail").hide();
+			$("#p_curled_tail").fadeIn(seconds).show();
+   			return false;
+		}else if(ui.value == 10){
+			$("#p_curled_tail").hide();
+			$("#o_straight_tail").hide();
+			$("#p_straight_tail").fadeIn(seconds).show();
+   			return false;
+		}else if(ui.value == 20){
+			$("#p_curled_tail").hide();
+			$("#p_straight_tail").hide();
+			$("#o_straight_tail").fadeIn(seconds).show();
+   			return false;
+		}
+	}
 	});
+	
+	
+/*	//slider two -- tail
+	$('#slider2').slider({
+		value: 0,   //starting point of slider
+		min: 0,
+		max: 20,   //ending amount
+		step: 10,   //snap to grid amount
+		animate: 'slow',
+		slide: function( event, ui ) { //put text at label
+		//$( ".label1" ).text(ui.value );
+		},
+		change: function(event, ui){
+		
+		if (ui.value == 0){
+			$('#femalemonster img').css('z-index', 'overlay');
+			$("#femalemonster img").html('<img src="images/FemaleMonster/female_p_curled_tail.png"/>');
+   			return false;
+		}else if(ui.value == 10){
+			$('#femalemonster img').css('z-index', 'overlay');
+			$("#femalemonster img").html('<img src="images/FemaleMonster/female_p_straight_tail.png"/>');
+   			return false;
+		}else if(ui.value == 20){
+			$('#femalemonster img').css('z-index', 'overlay');
+			$("#femalemonster img").html('<img src="images/FemaleMonster/female_o_straight_tail.png"/>');
+   			return false;
+		}
+	}
+	});
+*/
 	
 });
