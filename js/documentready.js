763983400957a5ffe420176d3014566e2286ecaa
Shannon Eric Peevey <peeveys@unlv.nevada.edu>
2014-02-24 17:09:55 -0800

    getTailImageName() function added as template for getting the name for additional bodyparts

--- js/documentready.js
+++ js/documentready.js
@@ -13,17 +13,23 @@ var femaleTraits = data.traits;
 // set male traits and shuffle genotypes
 var maleTraits = data.traits;
 
+// body colors
+var bodyColors = data.bodycolor;
+
 // maleTraitObject
 var maleTraitObject = {};
-console.log(data.traits);
-console.log(maleTraits);
-$.each(maleTraits, function(i, v){
-console.log(v.name);
-    console.log(shuffle(v.genotype));
-    maleTraitObject[v.name] = v.genotype[0];
 
+// set a few defaults for male
+maleTraitObject['gender'] = 'male';
+maleTraitObject['bodycolor'] = shuffle(bodyColors)[0];
+
+
+// we shuffle all of the genotypes for each trait
+// so that we can generate the male
+$.each(maleTraits, function(i, v){
+    shuffle(v.genotype);
+    maleTraitObject[v.name] = v.genotype[0];
 });
-    console.log( maleTraitObject);
 
 $(document).ready(function() {
     //reload page
@@ -53,6 +59,31 @@ $(document).ready(function() {
         $('#helpMessage').dialog('close');
         $('#divBottom').removeClass('hide');
     });
+
+    // build our male monster
+    console.log( maleTraitObject);
+    console.log(maleTraitObject['Tail Shape'][Object.keys(maleTraitObject['Tail Shape'])]);
+    
+    // tail
+    // male_tail_o/p_curled/straight.png
+    var maleTail = getTailImageName(maleTraitObject);
+    console.log(maleTail);
+    // body
+
+    // horn
+
+    // toes
+    
+    // claws
+
+    // ears
+
+    // teeth
+
+    // eye
+
+    //eyelid
+
 });
 
 function openHelpDialog() {
@@ -98,11 +129,52 @@ $('#closewelcome').tooltip();
 // HEAD
 //gender, bodyType, color, shape
 //image name generator
-function setImageName(gender, bodyType, color, shape){
-	
+function setImageName(bodyTraits) {
+    // Object
+    // Claws: Object
+    // Color-Blindness: Object
+    // Ear Shape: Object
+    // Eye: Object
+    // Eye Color (incomplete): Object
+    // Feet: Object
+    // Horn Color: Object
+    // Skin Color (codominant): Object
+    // Tail: Object
+    // Tail Color: Object
+    // Tail Shape: Object
+    // Teeth: Object
+    
+    // start our file name
+    // gender_bodypart_shape_size_color_number.png
+    // male_toes_na_na_b_4.png
+    // male_body_na_na_g_na.png
+
+    // walk through each part of the name
+
+    // gender
+    var filename = bodyTraits['gender'] + '_';
+
+    // bodypart
+    filename = filename + bodyTraits['bodycolor'] + '_';
+    
+    // shape
+    console.log(bodyTraits['Tail Shape']);
+    // if (bodyTraits['shape']
+    // ])
+    
 }
 
-//
+function getTailImageName(bodyTraits) {
+    // returns the name of the tail image
+    // gender
+    var filename = bodyTraits['gender'] + '_';
+    // color
+    filename = filename + bodyTraits['Tail Color'][Object.keys(bodyTraits['Tail Color'])].image + '_';
+    // shape
+    filename = filename + bodyTraits['Tail Shape'][Object.keys(bodyTraits['Tail Shape'])].image + '_tail.png';
+
+    return filename;
+}
 
 function getData(jsonfile){
     return $.ajax({
